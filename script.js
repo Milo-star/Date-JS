ad93190feb1b40ec5e20e8b640f181c898129719
Milo Bonnet <milovtt@gmail.com>
2022-12-13 11:36:52 +0100

    Exercise 2.2

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ let date = new Date;
 aryIannaTimeZones.forEach((timeZone) =>
 {
   let strTime = date.toLocaleString("en-US", {timeZone: `${timeZone}`});
-  console.log(timeZone, strTime);
+  console.log(timeZone, strTime); 
 });
 
-// Exercise 2
+// Exercise 2.1
 let date_1 = new Date('08/13/2003');
 let date_2 = new Date();
 const days = (date_1, date_2) =>{
@@ -22,3 +22,12 @@ const days = (date_1, date_2) =>{
     return TotalDays;
 }
 console.log("Il y a " + days(date_1, date_2) +" jours entre mon anniversaire et maintenant.");
+
+// Exercise 2.2
+function ShowDivTime(){
+  let date = new Date(document.getElementById('date').valueAsNumber);
+  let today = new Date();
+  document.getElementById('div_date').textContent = (today.getTime() - date.getTime())/(1000 * 60 * 24);
+}
+
+setInterval(ShowDivTime, 1000)
