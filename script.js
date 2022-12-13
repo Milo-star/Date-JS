6ed6b84927252b45c89a698c00fb25d0917c98ab
Milo Bonnet <milovtt@gmail.com>
2022-12-13 10:03:15 +0100

    exercice 2.1

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ aryIannaTimeZones.forEach((timeZone) =>
   let strTime = date.toLocaleString("en-US", {timeZone: `${timeZone}`});
   console.log(timeZone, strTime);
 });
+
+// Exercise 2
+let date_1 = new Date('08/13/2003');
+let date_2 = new Date();
+const days = (date_1, date_2) =>{
+    let difference = date_1.getTime() - date_2.getTime();
+    let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
+    return TotalDays;
+}
+console.log("Il y a " + days(date_1, date_2) +" jours entre mon anniversaire et maintenant.");
