1c952276ce7677b73c71fd7adca2613af678658b
Milo Bonnet <milovtt@gmail.com>
2022-12-13 16:17:19 +0100

    Ex 4

---
+++ ex 4 (Bonus)/assets/script.js
@@ -0,0 +1,14 @@
+let clock_mode = 1;
+
+function time(){
+    let now = new Date();
+    let days = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
+    let months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
+
+    let day = document.getElementById("day")
+    let mois = document.getElementById("mois")
+    let ans = document.getElementById("ans")
+    let min = document.getElementById("minute")
+    let sec = document.getElementById("seconde")
+    
+}
